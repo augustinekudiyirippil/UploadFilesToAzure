e349e52a7a499c4c230cefca2e0d0d2851beb9f1
augustinekudiyirippi <augustinekudiyirippil@gmail.com>
2024-07-23 11:43:44 +0100

    Added code to create folder in Azure

---
+++ AppToCopyFiles/AppToCopyFiles/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppToCopyFiles
+{
+    public static class StringExtension
+    {
+     
+
+        public static string[]  xSplit(string str, string sep)
+        {
+            return str.Split(new[] { sep }, StringSplitOptions.None);
+        }
+    }
+}
